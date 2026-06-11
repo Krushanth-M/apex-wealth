0cf36d5e4bcdc9e43bcf1dea068831e40d05a276
Krush <krush@example.com>
2026-06-11 23:20:39 +0530

    Configure BUILD_FOR_PAGES env to segregate GitHub Pages and Vercel base paths

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS === 'true' ? '/apex-wealth/' : '/',
+  base: process.env.BUILD_FOR_PAGES === 'true' ? '/apex-wealth/' : '/',
   plugins: [
     react(),
     tailwindcss(),
